97dc5132badcd7eb6d7b33d9b1557f57c8bb503e
yusei masuda <masuda.no@icloud.com>
2021-11-17 16:15:54 +0900

    タスクの新規作成画面、編集画面を作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,6 +14,23 @@ class TasksController < ApplicationController
   def edit
   end
 
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "Task was successfully created!"
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "Task was successfully updated!"
+    else
+      render :edit
+    end
+  end
+
 
 
 
