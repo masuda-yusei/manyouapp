35bba0d171a11af2ab74fb84e38362f943bc70c9
yusei masuda <masuda.no@icloud.com>
2021-11-19 01:17:56 +0900

    タスク詳細画面修正

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -19,6 +19,7 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
+        task = FactoryBot.create(:task, title: 'task')
         visit tasks_path
         expect(page).to have_content 'task'
       end
@@ -34,6 +35,7 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
        it '該当タスクの内容が表示される' do
+        task = FactoryBot.create(:task)
         visit task_path(task.id)
         expect(page).to have_content 'test_title'
        end
